e3518f844231f334a3b536748a0fe567db4735b6
Chae Yubin <cyoobin@naver.com>
2024-10-15 13:45:15 +0900

    feat: 홈 페이지에서 지역별 날씨 조회하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,35 @@
 import Head from "next/head";
 import Link from "next/link";
 import s from "../styles/Home.module.css";
+import { CurrentWeatherResponse } from "@/types/CurrentWeatherResponse";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+
+const locationList = [
+  { location: "seoul", locationKR: "서울" },
+  { location: "london", locationKR: "런던" },
+  { location: "paris", locationKR: "파리" },
+];
+
+export const getServerSideProps = async () => {
+  const locationEN = locationList.map((x) => x.location);
+  const apiPath = `http://api.weatherapi.com/v1/current.json?lang=ko&key=${API_KEY}`;
+
+  const currentWeather = await Promise.all(
+    locationEN.map(async (location) => {
+      const res = await fetch(`${apiPath}&q=${location}`);
+      const data: CurrentWeatherResponse = await res.json();
+      return data.current.condition.text;
+    })
+  );
+
+  return { props: { currentWeather } };
+};
+
+export default function Home({
+  currentWeather,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Head>
@@ -12,16 +39,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
+        <h1>날씨 앱</h1>
         <ul className={s.list}>
-          <li>
-            <Link href="/Seoul">서울</Link>
-          </li>
-          <li>
-            <Link href="/London">런던</Link>
-          </li>
-          <li>
-            <Link href="/Paris">파리</Link>
-          </li>
+          {locationList.map((location, index) => (
+            <li key={location.location}>
+              <span className={s.boldText}>{location.locationKR}의 날씨</span>는{" "}
+              {currentWeather[index]}
+              <br />
+              <Link href={`${location.location}`}>
+                <p className={s.aText}>3일 예보 확인하기</p>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </>
